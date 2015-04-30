2576db2d2e958845283252d47a1be5d044f599ef
Ernest Walker <jernestwalker@gmail.com>
2015-04-30 08:39:54 -0400

    first commit

---
+++ student_teams.rb
@@ -0,0 +1,10 @@
+=begin 
+
+Write a program that splits the class into teams of 2. 
+If there is an odd number of students the last group should be a group of 3.
+
+=end
+
+# Input names into an array
+students = ["Brandon", "Stephanie", "Mandy", "Jesse", "Ernest", "Mike", "Sabeel"]
+
