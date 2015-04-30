9c0c65282fa4aa681252e2b32687b943698aec30
Ernest Walker <jernestwalker@gmail.com>
2015-04-30 08:56:13 -0400

    paried up students

--- student_teams.rb
+++ student_teams.rb
@@ -6,5 +6,23 @@ If there is an odd number of students the last group should be a group of 3.
 =end
 
 # Input names into an array
-students = ["Brandon", "Stephanie", "Mandy", "Jesse", "Ernest", "Mike", "Sabeel"]
+students = ["Brandon", "Stephanie", "Enoch", "Mandy", "Jesse", "Ernest", "Jamel", "Sabeel", "Mike"]
+
+# shuffle or randomize the array
+students.shuffle!
+
+# set a count
+count = 0
+
+# set a number of students
+num_students = students.length - 1
+
+until count == num_students
+
+	puts "#{students[count]} & #{students[count+1]}"
+
+	count += 2
+
+end
+
 
